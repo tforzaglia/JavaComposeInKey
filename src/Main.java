b209283811ec987e2dd6c0d6c17070e3ea22431c
Tom Forzaglia <thomas.forzaglia@gmail.com>
2012-06-01 23:06:36 -0400

    added action listeners to buttons

--- src/Main.java
+++ src/Main.java
@@ -6,41 +6,69 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-
 public class Main {
 	
 	public static void main(String[] args) {
-		//MajorDisplay keyname = new MajorDisplay();
 		
 		//Create the label and buttons for major keys
 		JLabel major = new JLabel("MAJOR");
 		JButton aButton = new JButton("A");
-		JButton asButton = new JButton("A# / Bb");
+		JButton asButton = new JButton("A#");
 		JButton bButton = new JButton("B");
 		JButton cButton = new JButton("C");
-		JButton csButton = new JButton("C# / Db");
+		JButton csButton = new JButton("C#");
 		JButton dButton = new JButton("D");
-		JButton dsButton = new JButton("D# / Eb");
+		JButton dsButton = new JButton("D#");
 		JButton eButton = new JButton("E");
 		JButton fButton = new JButton("F");
-		JButton fsButton = new JButton("F# / Gb");
+		JButton fsButton = new JButton("F#");
 		JButton gButton = new JButton("G");
-		JButton gsButton = new JButton("G# / Ab");
+		JButton gsButton = new JButton("G#");
 		
 		//Create the label and buttons for minor keys
 		JLabel minor = new JLabel("MINOR");
 		JButton amButton = new JButton("Am");
-		JButton asmButton = new JButton("A#m / Bbm");
+		JButton asmButton = new JButton("A#m");
 		JButton bmButton = new JButton("Bm");
 		JButton cmButton = new JButton("Cm");
-		JButton csmButton = new JButton("C#m / Dbm");
+		JButton csmButton = new JButton("C#m");
 		JButton dmButton = new JButton("Dm");
-		JButton dsmButton = new JButton("D#m / Ebm");
+		JButton dsmButton = new JButton("D#m");
 		JButton emButton = new JButton("Em");
 		JButton fmButton = new JButton("Fm");
-		JButton fsmButton = new JButton("F#m / Gbm");
+		JButton fsmButton = new JButton("F#m");
 		JButton gmButton = new JButton("Gm");
-		JButton gsmButton = new JButton("G#m / Abm");
+		JButton gsmButton = new JButton("G#m");
+		
+		//create the action listener
+		GoToKeyInfo listener = new GoToKeyInfo();
+		
+		//add the action listener to all the buttons
+		aButton.addActionListener(listener);
+		asButton.addActionListener(listener);
+		bButton.addActionListener(listener);
+		cButton.addActionListener(listener);
+		csButton.addActionListener(listener);
+		dButton.addActionListener(listener);
+		dsButton.addActionListener(listener);
+		eButton.addActionListener(listener);
+		fButton.addActionListener(listener);
+		fsButton.addActionListener(listener);
+		gButton.addActionListener(listener);
+		gsButton.addActionListener(listener);
+		
+		amButton.addActionListener(listener);
+		asmButton.addActionListener(listener);
+		bmButton.addActionListener(listener);
+		cmButton.addActionListener(listener);
+		csmButton.addActionListener(listener);
+		dmButton.addActionListener(listener);
+		dsmButton.addActionListener(listener);
+		emButton.addActionListener(listener);
+		fmButton.addActionListener(listener);
+		fsmButton.addActionListener(listener);
+		gmButton.addActionListener(listener);
+		gsmButton.addActionListener(listener);
 		
 		//content JPanel is the content of the entire window
 		JPanel content = new JPanel();
@@ -48,7 +76,7 @@ public class Main {
 		//left is JPanel for the major keys
 		JPanel left = new JPanel();
 		
-		//right is JPanel for the minor keys
+		//right JPanel is for the minor keys
 		JPanel right = new JPanel();
 		
 		//use a Border Layout for the main window display to have the parts arranged side by side
@@ -61,6 +89,7 @@ public class Main {
 		content.add(right, BorderLayout.EAST);
 		
 		//left and right panels are set up with Box Layout to have the content displayed one on top of the other
+		
 		//add the title and buttons to the panel for major keys
 		left.setLayout(new BoxLayout(left, BoxLayout.PAGE_AXIS));
 		left.add(major);
@@ -76,7 +105,7 @@ public class Main {
 		left.add(fsButton);
 		left.add(gButton);
 		left.add(gsButton);
-	
+
 		//add the title and buttons to the panel for minor keys
 		right.setLayout(new BoxLayout(right, BoxLayout.PAGE_AXIS));
 		right.add(minor);
@@ -94,14 +123,17 @@ public class Main {
 		right.add(gsmButton);
 		
 		//create the main window for the application
-		JFrame window = new JFrame("Test");
+		JFrame window = new JFrame("Compose in Key -- Java Edition");
 		
-		//set the size large enough horizontally and vertically
+		//set the content JPanel (which contains both the right and left panels) as the content pane for the frame
 	    window.setContentPane(content);
+	    
+	    //set size large enough horizontally and vertically
 	    window.setSize(250,500);
+	    
 	    window.setLocation(100,100);
         window.setVisible(true);
 
 	   }//close main method
-}//close Main close
+}//close Main class
 
