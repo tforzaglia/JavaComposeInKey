d3ead74878f09c8c0776a2311acff9725febdd14
Tom Forzaglia <thomas.forzaglia@gmail.com>
2012-06-06 22:59:03 -0400

    added the remaining keys to the determineKey method

--- src/KeyInfo.java
+++ src/KeyInfo.java
@@ -1,11 +1,10 @@
+import java.awt.Color;
+
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-//have the button pressed string passed into the constructor
-//determine what label will be set based on the name of the button
-//set up the GUI for the window and set visible
 
 public class KeyInfo {
 	
@@ -35,7 +34,7 @@ public class KeyInfo {
 		}
 		else if(buttonName == "A#" || buttonName == "Gm") {
 			if(buttonName == "A#") 
-				keyName = new JLabel("A#");
+				keyName = new JLabel("A# Major");
 			else
 				keyName = new JLabel("G Minor");
 			noteList = new JLabel("A#  B  C#  D  E  F#  G#");
@@ -57,6 +56,73 @@ public class KeyInfo {
 			noteList = new JLabel("C  D  E  F  G  A  B");
 			chordList = new JLabel("Am  C  Dm  Em  F  G");
 		}
+		else if(buttonName == "C#" || buttonName == "A#m") {
+			if(buttonName == "C#") 
+				keyName = new JLabel("C# Major");
+			else
+				keyName = new JLabel("A# Minor");
+			noteList = new JLabel("C#  D#  F  F#  G#  A#  C");
+			chordList = new JLabel("C#  D#m  E#m  F#  G#  A#m");
+		}
+		else if(buttonName == "D" || buttonName == "Bm") {
+			if(buttonName == "D") 
+				keyName = new JLabel("D Major");
+			else
+				keyName = new JLabel("B Minor");
+			noteList = new JLabel("D  E  F#  G  A  B C#");
+			chordList = new JLabel("D  Em  F#m  G  A  Bm");
+		}
+		else if(buttonName == "D#" || buttonName == "Cm") {
+			if(buttonName == "D#") 
+				keyName = new JLabel("D# Major");
+			else
+				keyName = new JLabel("C Minor");
+			noteList = new JLabel("D#  F  G  G#  A#  C  D");
+			chordList = new JLabel("D#  Fm  Gm  G#  A#  Cm");
+		}
+		else if(buttonName == "E" || buttonName == "C#m") {
+			if(buttonName == "E") 
+				keyName = new JLabel("E Major");
+			else
+				keyName = new JLabel("C# Minor");
+			noteList = new JLabel("E  F#  G#  A  B C#  D#");
+			chordList = new JLabel("E  F#m  G#m  A  B  C#m");
+		}
+		else if(buttonName == "F" || buttonName == "Dm") {
+			if(buttonName == "F") 
+				keyName = new JLabel("F Major");
+			else
+				keyName = new JLabel("D Minor");
+			noteList = new JLabel("F  G  A  A# C  D  E");
+			chordList = new JLabel("F  Gm  Am  A#  C  Dm");
+		}
+		else if(buttonName == "F#" || buttonName == "D#m") {
+			if(buttonName == "F#") 
+				keyName = new JLabel("F# Major");
+			else
+				keyName = new JLabel("D# Minor");
+			noteList = new JLabel("F#  G#  A#  B  C#  D#  F");
+			chordList = new JLabel("F#  G#m  A#m  B  C#  D#m");
+		}
+		else if(buttonName == "G" || buttonName == "Em") {
+			if(buttonName == "G") 
+				keyName = new JLabel("G Major");
+			else
+				keyName = new JLabel("E Minor");
+			noteList = new JLabel("G  A  B  C  D  E  F#");
+			chordList = new JLabel("G  Am  Bm  C  D  Em");
+		}
+		else if(buttonName == "G#" || buttonName == "Fm") {
+			if(buttonName == "G#") 
+				keyName = new JLabel("G# Major");
+			else
+				keyName = new JLabel("F Minor");
+			noteList = new JLabel("G#  A#  C  C#  D#  F  G");
+			chordList = new JLabel("G#  A#m  Cm  C#  D#  Fm");
+		}
+		keyName.setForeground(Color.RED);
+		noteList.setForeground(Color.WHITE);
+		chordList.setForeground(Color.WHITE);
 	}
 	
 	//method to build the GUI 
@@ -64,10 +130,13 @@ public class KeyInfo {
 		
 		//static labels -- same for every key
 		JLabel noteTitle = new JLabel("Notes in the Scale");
+		noteTitle.setForeground(Color.BLUE);
 		JLabel chordTitle = new JLabel("Chords in the Key");
+		chordTitle.setForeground(Color.BLUE);
 		
 		JPanel infoPanel = new JPanel();
 		infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.PAGE_AXIS));
+		infoPanel.setBackground(Color.GRAY);
 		
 		//add components to the panel
 		infoPanel.add(keyName);
